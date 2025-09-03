39f669642253fc9c4ec0749c374b7c7bf25b99cb
Francisco Santillana <120747365+fasantillana@users.noreply.github.com>
2025-09-03 16:22:58 -0400

    Update Hello_World.py
    added comments to remember what I did and how the functions work together.

--- Hello_World.py
+++ Hello_World.py
@@ -1,9 +1,12 @@
-print('Hello World')
-print('What is your name?') # Ask for their name
-my_name = input('>')
-print('It is good to meet you, ' + my_name)
-print('The length of your name is:')
+print("Hello World")
+print("What is your name?")  # Ask for their name
+my_name = input(">")
+print("It is good to meet you, " + my_name)
+print("The length of your name is:")
 print(len(my_name))
-print('What is your age?') # Ask for their age
-my_age = input('>')
-print('You will be ' + str(int(my_age) + 1) + ' in a year.')
+print("What is your age?")  # Ask for their age
+my_age = input(">")
+#The code below works like PEMDAS where parenthesis will be evaluated first. 
+#The function int(my_age) is taking my string value and converting it to interger, then it adds +1
+#The function str() then converts my interger number back to string before it concactinates it 
+print("You will be " + str(int(my_age) + 1) + " in a year.")
