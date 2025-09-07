9b2d6343b3a5dfdcf5230df51afb6a85f6b0a116
Francisco Santillana <120747365+fasantillana@users.noreply.github.com>
2025-09-06 21:43:16 -0400

    Kirk Byer's Lesson 1 exercises

---
+++ Lesson1.py
@@ -0,0 +1,7 @@
+#Exercise 1
+sf_gw1 = "172.31.255.1/24"
+sf_gw2 = input('what is your gw2 IP address?:\n>')
+headerline = '-' * 20
+header1 = 'sf_gw1'
+header2 = 'sf_gw2'
+print(f'{header1:^20}{header2:^20}"\n"{headerline:^} {headerline:^}"\n"{sf_gw1:^20}{sf_gw2:^20}')
