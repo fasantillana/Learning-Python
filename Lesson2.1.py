214e4cde2c954db3a6e051ccbeaf8c36d55682f5
Francisco <francisco.santillana@outlook.com>
2025-09-13 20:32:29 -0400

    Lesson2 Exercise 1

---
+++ Lesson2.1.py
@@ -0,0 +1,5 @@
+#Kirk Byers' Lesson 2 - Exercise 1
+base_addr = '192.168.254.'
+submask1 = int(input('Please insert subnet prefix length within range 25-30:\n >')) #prompted user for slash notation and saved as int
+
+
