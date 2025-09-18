b11fa50cccf625b4d1c31bf60e625f3704dd8c57
Francisco <francisco.santillana@outlook.com>
2025-09-18 17:44:26 -0400

    Lesson2, Exercise2 completed

--- Lesson2.2.py
+++ Lesson2.2.py
@@ -1,8 +1,17 @@
 #Kirk Byers' PyNet Learning Python : Lesson 2 Exerice 2 
 
 #opened the file show_version.txt as read-only and passed all the output onto variable 'data'
-f = open('show_version.txt', mode='r')
-data = f.read()
+f = open("show_version.txt")
+data = f.readline() #using f.readline over .read() would print just the first line
 
-print(f.readline())
+# the following is not a valid way to use pring:  print(f.readline())
+print(data)
 print(type(data))
+f.close()
+
+#Doing the same task as above but this time I am using with to create a context manager (which is useful to contain errors
+with open("show_version.txt") as f: 
+    data=f.readline()
+    print(data)
+    print(type(data))
+
